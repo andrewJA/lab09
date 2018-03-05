9a50ac8eec89505019aa7dc527857cd05834d5f9
andreyja <quietbbl@gmail.com>
2018-03-05 23:01:25 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,11 @@
 #include <fstream>
 #include <iostream>
 
+///
+///1st print function
+///
 void print(const std::string& text, std::ostream& out = std::cout);
+///
+///2nd print function
+///
 void print(const std::string& text, std::ofstream& out);
